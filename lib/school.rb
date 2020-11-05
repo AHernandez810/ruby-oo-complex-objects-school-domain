0d50a2e4fda8f2b3f8d03ec208419d7945747bb7
ashley <ashleyhernandez810.ah@gmail.com>
2020-11-05 06:34:19 -0500

    commit tree 37de3d2366839f51d5acf1f74c2e23b32c483199

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class School
+
+    def initialize(name)
+        @name = name 
+        @roster = {}
+    end 
+
+    def roster
+        @roster
+    end 
+
+    def add_student(student_name, grade)
+        @student_name = student_name
+        @grade = grade
+        if roster.include?(grade) == false
+        roster[grade] = []
+    end 
+        roster[grade] << student_name
+    end 
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |student_name, grade|
+            grade.sort!
+        end 
+    end 
+end 
+
+
